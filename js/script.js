8a0099c15c5f38b25de880d09bdab93a5dd47c41
Kamil Snietka <krevelir@gmail.com>
2022-12-29 14:46:59 +0100

    removing tasks

--- js/script.js
+++ js/script.js
@@ -9,19 +9,7 @@
             done: true,
         },
     ]
-
-    const render = () => {
-        let htmlString = ""
-        for (const task of tasks) {
-            htmlString += `
-            <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-            ${task.content}
-            </li>
-            `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
-
+    
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -30,11 +18,37 @@
         render();
     };
 
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+   
+    const render = () => {
+        let htmlString = ""
+        for (const task of tasks) {
+            htmlString += `
+            <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+            ${task.content}
+            <button class="js-remove">remove</button>
+            </li>
+            `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        })
+    }
+
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        console.log(newTaskContent);
 
         if (newTaskContent === "") {
             return;
@@ -42,8 +56,8 @@
 
         addNewTask(newTaskContent);
     }
-    
-    
+
+
     const init = () => {
         render();
 
@@ -54,60 +68,3 @@
 
     init();
 }
-
-
-
-
-/*
-
-{
-    const tasks = [
-        {
-            content: "complete a JS course module",
-            done: false,
-        },
-        {
-            content: "put some clothes on",
-            done: true,
-        },
-    ]
-
-    const render = () => {
-        let htmlString = "";
-       
-        for (const task of tasks) {
-            htmlString += `
-                <li ${task.done ? " style=\"text-decoration: line-through\"" : ""}> 
-                ${task.content}
-                </li>
-            `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
-    const init = () => {
-        render();
-        }
-        const form = document.querySelector(".js-form");
-
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            console.log(newTaskContent);
-
-            if (newTaskContent === "") {
-                return;
-            }
-        
-            tasks.push({
-                content: newTaskContent,
-                
-            })
-            render ();
-
-        init();
-    }
-}
-
-*/
